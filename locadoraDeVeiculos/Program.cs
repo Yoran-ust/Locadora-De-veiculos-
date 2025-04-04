dfb20e2da958a4b4b447d00494decd956894a956
Yoran-ust <johannc55@gmail.com>
2025-04-04 10:18:18 -0300

    atualização

--- locadoraDeVeiculos/Program.cs
+++ locadoraDeVeiculos/Program.cs
@@ -5,16 +5,12 @@ class Program
     static void Main(string[] args)
     {
         List<Veiculo> veiculos = new List<Veiculo>
-        {
-            new Carro("Corolla", "Toyota", 2022, 300m),
-            new Moto("XRE 300", "Honda", 2021, 150m),
-            new Caminhao("Scania R450", "Scania", 2020, 500m)
-        };
-        int dias = 5;
-        foreach (var veiculo in veiculos)
-        {
-            Console.WriteLine($"Veículo: {veiculo.Modelo} | Marca: {veiculo.Marca} | Ano: {veiculo.Ano} | Custo por {dias} dias: R$ {veiculo.CalcularAluguel(dias):F2}");
-        }
-            
+    {
+        new Carro("Corolla", "Toyota", 2022, 300),
+        new Moto("XRE 300", "Honda", 2021, 150),
+        new Caminhao("Scania R450", "Scania", 2020, 500),
+        new Carro("Civic", "Honda", 2023, 320)
+    };
+
     }
 }
